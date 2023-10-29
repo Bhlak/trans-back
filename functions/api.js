6a1ad81bff244eda05307f302be299c74a708ea2
Bhlak <bhlakcodes@proton.me>
2023-10-30 00:01:16 +0100

    Environment fix

--- functions/api.js
+++ functions/api.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-// const serverless = require('serverless-http');
+const serverless = require('serverless-http');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoute = require('../routes/auth');
-const cors = require('cors');
 const productRoute = require('../routes/products');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -17,14 +17,13 @@ mongoose.connect(process.env.DB_CONN).then(() => {
 
 app.use(express.json());
 app.use(cors());
-// app.use(`${process.env.NTL_ROUTE}/auth`, authRoute);
-app.use('/api/auth', authRoute);
-app.use('/api/products', productRoute);
+app.use(`${process.env.NTL_ROUTE}/auth`, authRoute);
+app.use(`${process.env.NTL_ROUTE}/products`, productRoute);
 
 // BASE URL FOR API: https://jazzy-vacherin-4c19af.netlify.app/
 
-app.listen(4000, () => {
-  console.log('Listening on port 4000');
-});
+// app.listen(4000, () => {
+//   console.log('Listening on port 4000');
+// });
 
-// module.exports.handler = serverless(app);
+module.exports.handler = serverless(app);
